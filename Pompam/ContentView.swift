a43c95d7a6a7c0ba010334ff4538dc69e37df441
Hendra Lijaya <hendralijaya@gmail.com>
2024-03-30 20:17:37 +0700

    Initial Commit

---
+++ Pompam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pompam
+//
+//  Created by hendra on 30/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pompam/PompamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PompamApp.swift
+//  Pompam
+//
+//  Created by hendra on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PompamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
