b6b9715d62db74204eb9b71bee64893ffc77cc33
Hendra Lijaya <hendralijaya@gmail.com>
2024-04-04 20:15:38 +0700

    Not done any good things

---
+++ Pompam/Core/Utils/Router.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  Pompam
+//
+//  Created by hendra on 03/04/24.
+//
+
+import Foundation
+
+@main
+struct RoutingApp: App {
+    
+}
---
+++ Pompam/Feature/Component/Page/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Pompam
+//
+//  Created by hendra on 02/04/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
